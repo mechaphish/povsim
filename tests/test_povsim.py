d69bcc7a2c98886137d47e02d4baf9bbfbd4ee0f
Nick Stephens <nick.d.stephens@gmail.com>
2016-07-04 23:09:53 -0700

    Add simple testcase for povsim

---
+++ tests/test_povsim.py
@@ -0,0 +1,47 @@
+import nose
+from povsim import CGCPovSimulator
+
+import os
+bin_location = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../binaries-private'))
+pov_location = str(os.path.dirname(os.path.realpath(__file__)))
+
+import logging
+logging.getLogger("povsim").setLevel("DEBUG")
+
+def test_good_pov():
+    '''
+    Test exploitation of legit_00001 given a good crash.
+    '''
+
+    pov_tester = CGCPovSimulator()
+    pov_path = os.path.join(pov_location, 'good.pov')
+    binary_path = os.path.join(bin_location, "cgc_scored_event_1/cgc/0b32aa01_01")
+    result = pov_tester.test_binary_pov(pov_path, binary_path, enable_randomness=True)
+
+    nose.tools.assert_true(result)
+
+def test_bad_pov():
+    '''
+    Test exploitation of legit_00001 given a good crash.
+    '''
+
+    pov_tester = CGCPovSimulator()
+    pov_path = os.path.join(pov_location, 'bad.pov')
+    binary_path = os.path.join(bin_location, "cgc_scored_event_1/cgc/0b32aa01_01")
+    result = pov_tester.test_binary_pov(pov_path, binary_path, enable_randomness=True)
+
+    nose.tools.assert_false(result)
+
+def run_all():
+    functions = globals()
+    all_functions = dict(filter((lambda (k, v): k.startswith('test_')), functions.items()))
+    for f in sorted(all_functions.keys()):
+        if hasattr(all_functions[f], '__call__'):
+            all_functions[f]()
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) > 1:
+        globals()['test_' + sys.argv[1]]()
+    else:
+        run_all()
